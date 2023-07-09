5c8b35bb605112675a2ae85a2b30f535e1d7e38b
laodeshaldanfalih <77shaldan77@gmail.com>
2023-07-09 22:15:06 +0700

    add laporan kehilangan page & vehicle data backend

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // monggose db
-mongoose.connect('mongodb+srv://admin-shaldan:admin123@cluster0.cpnwcn2.mongodb.net/RanmITSDB');
+// mongoose.connect('mongodb+srv://admin-shaldan:admin123@cluster0.cpnwcn2.mongodb.net/RanmITSDB');
 
 app.get("/", (req,res)=>{
     res.render("homepage");
@@ -17,4 +17,24 @@ app.get("/", (req,res)=>{
 
 app.listen(3000,()=>{
     console.log("Running app on port 3000");
-})
+});
+
+app.get("/laporan-kehilangan-page", (req,res)=>{
+    res.render('laporan-kehilangan-page')
+});
+
+app.post("/laporan-kehilangan-page", (req,res)=>{
+    const vehicleData = {
+        handphoneNumber : req.body.handphoneNumber,
+        vModel: req.body.vModel,
+        vYears: req.body.vYears,
+        vColor: req.body.vColor,
+        vNumber: req.body.vNumber,
+        vPhoto: req.body.vPhoto,
+        lostTime: req.body.lostTime,
+        lostLocation: req.body.lostLocation,
+        description: req.body.description
+    }
+    console.log(vehicleData);
+    res.redirect("/");
+});
